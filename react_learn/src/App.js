6ec4fb3083749e6013ddfe8d3eb30a77b43d1782
JanakiramYellapu <jhanu94@gmail.com>
2020-06-03 20:19:01 +0530

    Setting up completed

---
+++ react_learn/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi Hello Namaste </h1>
+      <Person/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_learn/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = ()=>{
+    return <p> I'm learning React.</p> 
+}
+
+export default person;
