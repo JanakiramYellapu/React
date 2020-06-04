b4b16374890e64b01b3577586748af48d321cc16
JanakiramYellapu <jhanu94@gmail.com>
2020-06-04 23:46:23 +0530

    componentsAndStates

--- react_learn/src/App.js
+++ react_learn/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hi Hello Namaste </h1>
-      <Person/>
-    </div>
-  );
+class App extends Component {
+  state ={
+    persons :[
+      {name : "Janakiram", age: 26},
+      {name : "vinay", age : 28}
+    ]
+  }
+
+  switchNameHandler = ()=>{
+    // console.log("Hello")
+    this.setState({
+      persons :[
+        {name : "Janakiram Yellapu", age:25},
+        {name : "Vinay", age : 23}
+      ]
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1> Hi Hello Namaste </h1>
+        <button onClick ={this.switchNameHandler}>Switch Name </button>
+        <Person name = {this.state.persons[0].name} age ={this.state.persons[0].age}> I'm learining </Person>
+        <Person name ={this.state.persons[1].name} age ={this.state.persons[1].age}></Person>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react_learn/src/Person/Person.js
+++ react_learn/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = ()=>{
-    return <p> I'm learning React.</p> 
+const person = (props)=>{
+    return (
+        <div>
+            <p>My name is {props.name} age is {props.age} </p>
+            <p>{ props.children }</p>
+        </div>
+    ) 
 }
 
 export default person;
