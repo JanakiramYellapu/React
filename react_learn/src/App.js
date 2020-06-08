267068cdaaf3bb8b090ee5b3f1f3012ecffe3028
JanakiramYellapu <jhanu94@gmail.com>
2020-06-08 22:23:06 +0530

    Debugging completed

--- react_learn/src/App.js
+++ react_learn/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from  './ErrorBoundary/ErrorBoundary';
+
 
 class App extends Component {
   state ={
@@ -44,11 +46,15 @@ class App extends Component {
 
   render(){
     const style = {
-      backgroundColor : 'white',
+      backgroundColor : 'green',
       font : 'inherit',
       border : '1px solid blue',
       padding: '8px',
-      cursor : 'pointer'
+      cursor : 'pointer',
+      ':hover' :{
+        backgroundColor:'lightgreen',
+        color :'white'
+      }
     };
 
     let persons = null
@@ -56,16 +62,22 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map( (person, index) =>{
-              return <Person
+              return <ErrorBoundary key = {person.id}>
+                      <Person
                       click = {() => this.deleteNameHandler(index)}
                        name = {person.name}
-                        age = {person.age} 
-                        key = {person.id}
+                        age = {person.age}
                         changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+                      </ErrorBoundary>
             })
           }
         </div> 
       );
+      style.backgroundColor = "red";
+      style[':hover'] = {
+        backgroundColor:'lightgreen',
+        color :'white'
+      };
     }
 
     return (
